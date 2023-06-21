f21bd096c4e3fc9d6cd85c427056f56d05f5275d
alyssabenipayo <alymaebenipayo@gmail.com>
2023-06-21 16:58:21 -0700

    Completed Week 1 exercise. Updated readme.

--- cs50-intro-comp-sci/population/population.cs
+++ cs50-intro-comp-sci/population/population.cs
@@ -1,31 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Assignment completed in the Harvard CS50 codespa
 int main(void)
 {
-    // Run program with ./population
+    // Compile by running make population
     // TODO: Prompt for start size
     int startSize;
-    do {
-        startSize = get_int("Enter start size: \n")
-        printf("Start Size: \n", n);
+    do
+    {
+        startSize = get_int("Enter start size: \n");
+        printf("Start Size: %i\n", startSize);
     }
-    while (n < 9);
+    while (startSize < 9);
 
     // TODO: Prompt for end size
     int endSize;
-    do {
-        endSize = get_int("Enter end size: \n")
-        printf("End Size: \n", n);
-        }
-        while (endSize < startSize);
+    do
+    {
+        endSize = get_int("Enter end size: \n");
+        printf("End Size: %i\n", endSize);
+    }
+    while (endSize < startSize);
 
     // Keep track of number of years
     int years = 0;
 
     // TODO: Calculate number of years until we reach threshold
     // Update population until we reach threshold
-    while (startSize < endSize) {
+    while (startSize < endSize)
+    {
         startSize = startSize + (startSize / 3) - (startSize / 4);
         // Update the # of years that went by. This will increase years by 1.
         years++;
