0ad5ffb500d91bf94569c5c8e973034d507f8617
alyssabenipayo <alymaebenipayo@gmail.com>
2023-06-22 19:42:05 -0700

    Mario-more completed :)

--- cs50-intro-comp-sci/mario-more/mario.c
+++ cs50-intro-comp-sci/mario-more/mario.c
@@ -1,13 +1,41 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void half_pyramid(int n);
+int height;
+
+int main(void)
 {
-    int height;
     do
     {
         height = get_int("Enter the height: \n");
-        printf("Height: %i\n", height);
     }
-    while (height > 1 && height < 8);
+    while (height < 1 || height > 8);
+    half_pyramid(height);
+}
+
+void half_pyramid(int n)
+{
+    // this will add a new line
+    for (int i = 0; i < height; i++)
+    {
+        // This loop will add the spaces
+        for (int s = height - i; s > 1; s--)
+        {
+            printf(" ");
+        }
+        // Left hashies (h)
+        for (int h = 0; h <= i; h++)
+        {
+            printf("#");
+        }
+        // Adding the gaps
+        printf("  ");
+        // Right hashies (h)
+        for (int h = 0; h <= i; h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
