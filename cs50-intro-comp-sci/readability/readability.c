1cfb291524c525e7d929c25cb5701311561bb9ab
alyssabenipayo <alymaebenipayo@gmail.com>
2023-08-07 22:59:36 -0700

    Problem 2

---
+++ cs50-intro-comp-sci/readability/readability.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int count_letters(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    printf("%s", userInput);
+}
+
+int count_letters(string text)
+{
+    int countText = strlen(text);
+    printf("%s letter", countText);
+}
