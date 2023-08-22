961d246c040dac03275b94ccaaa1ba952ef49634
alyssabenipayo <alymaebenipayo@gmail.com>
2023-08-21 23:04:28 -0700

    Problem set: Readability completed.

--- cs50-intro-comp-sci/readability/readability.c
+++ cs50-intro-comp-sci/readability/readability.c
@@ -1,17 +1,32 @@
 #include <cs50.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
-
-int count_letters(string text);
+#include <string.h>
 
 int main(void)
 {
-    string text = get_string("Text: ");
-    printf("%s", userInput);
-}
+    string userInput = get_string("Text: ");
+    int count_words, count_sentences, count_letters;
+    count_words = count_sentences = count_letters = 0;
+    for (int i = 0, len = strlen(userInput); i < len; i++)
+    {
+        if (isalpha(userInput[i]))
+            count_letters++;
+        if ((i == 0 && userInput[i] != ' ') || (i != len - 1 && userInput[i] == ' ' && userInput[i + 1] != ' '))
+            count_words++;
+        if (userInput[i] == '.' || userInput[i] == '?' || userInput[i] == '!')
+            count_sentences++;
+    }
 
-int count_letters(string text)
-{
-    int countText = strlen(text);
-    printf("%s letter", countText);
+    float L = (count_letters / (float) count_words) * 100;
+    float S = (count_sentences / (float) count_words) * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+        printf("Before Grade 1\n");
+    else if (index >= 16)
+        printf("Grade 16+\n");
+    else
+        printf("Grade %i\n", index);
 }
